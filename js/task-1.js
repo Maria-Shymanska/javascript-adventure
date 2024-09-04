7ed52c62fa891065033caf61bceefd5e724ce5ae
Shymanska_Maria <shymanskam524@gmail.com>
2024-09-04 19:58:02 +0200

    update task -1

---
+++ js/task-1.js
@@ -0,0 +1,66 @@
+
+// Доповни код, присвоївши змінній totalPrice вираз для підрахунку загальної суми замовлення.Змінна pricePerItem зберігає ціну за одиницю товару, а orderedQuantity - кількість одиниць товару в замовленні.
+
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// const totalPrice = pricePerItem * orderedQuantity;
+
+// console.log(totalPrice);    // 14000
+
+
+// Змінні та типи даних
+
+
+// Оператор typeof використовується для визначення типу даних значення або виразу.
+
+// const quantity = 17;
+// console.log(typeof quantity); // виведе "number"
+
+// const message = "JavaScript is awesome!";
+// console.log(typeof message); // виведе "string"
+
+// const isSidebarOpen = false;
+// console.log(typeof isSidebarOpen); // виведе "boolean"
+
+// let username;
+// console.log(typeof username); // виведе "undefined"
+
+// let status = null;
+// console.log(typeof status); // виведе "object"
+
+
+
+// Додай код, який записує у змінні наступні значення:
+
+// courseTopicLength - довжина рядка
+// firstElement - перший символ рядка
+// lastElement - останній символ рядка (використовуй змінну courseTopicLength)
+
+
+// const courseTopic = "JavaScript essentials";
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopicLength - 1];
+
+
+// console.log(courseTopicLength); // 21
+// console.log(firstElement); // J
+// console.log(lastElement); // s
+
+
+// Оператори порівняння
+
+// Оголоси змінну isValid і задай їй значенням результат перевірки рівності паролей в змінних correctPassword і userPassword.
+
+// Якщо паролі співпадають (сувора рівність), значення isValid має бути true
+// Якщо паролі не співпадають, значення isValid має бути false
+
+// const correctPassword = "jqueryismyjam";
+// const userPassword = "mangodab3st";
+// const isValid = correctPassword === userPassword;
+
+
+// console.log(isValid);  // false
