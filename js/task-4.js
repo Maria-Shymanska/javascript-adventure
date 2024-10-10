47681f33055c785f24ec472552deb8c73fc66e39
Shymanska_Maria <shymanskam524@gmail.com>
2024-10-10 23:10:26 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -42,6 +42,25 @@
 // const values = Object.values(book);
 // console.log(values); // ["The Last Kingdom", "Bernard Cornwell", 8.38]
 
+// методи обєкта
+
+// ✅ Логічно й синтаксично згруповані сутності
+const bookShelf = {
+  books: ["The Last Kingdom", "Dream Guardian"],
+  // Це метод об'єкта
+  getBooks() {
+    return "Returning all books";
+  },
+  // Це метод об'єкта
+  addBook(bookName) {
+		return `Adding book ${bookName}`;
+  },
+};
+
+// Виклики методів
+bookShelf.getBooks(); // поверне "Returning all books"
+bookShelf.addBook("New book 1"); // поверне "Adding book New book 1"
+bookShelf.addBook("New book 2"); // поверне "Adding book New book 2"
 
 
 
